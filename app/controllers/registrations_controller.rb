f987679c4436124b78419e3a0a6c923fa9b15e02
James Grieve <pocojames@hotmail.com>
2020-09-27 01:43:10 -0700

    add name to signup form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,2 @@
 class RegistrationsController < ApplicationController
-    private
-    def sign_up_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
 end
